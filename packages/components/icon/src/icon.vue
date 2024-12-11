640c492de376f59369d3ef0ed9fc988eb5702c34
v_jpch <v_jpch.digitalgd.com>
2024-12-11 11:39:35 +0800

    feat: 编写icon组件模板

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <i :class="bem.b()" :style="styles">
+    <slot></slot>
+  </i>
+</template>
+
+<script setup lang="ts">
+import { computed, CSSProperties } from "vue";
+import { createNamespace } from "@cjp-ui/utils/create";
+import { iconProps } from "./icon";
+
+// 使用bem规范
+const bem = createNamespace("icon");
+
+// 定义props
+const props = defineProps(iconProps);
+
+// 计算属性处理样式
+const styles = computed<CSSProperties>(() => {
+  if (!props.size && !props.color) return {};
+
+  return {
+    ...(props.size ? { "font-size": props.size + "px" } : {}),
+    ...(props.color ? { color: props.color } : {}),
+  };
+});
+</script>
