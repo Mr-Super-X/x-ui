0846234f02e24b3442e5c247391f6c0fb87c2223
v_jpch <v_jpch.digitalgd.com>
2024-12-11 11:56:49 +0800

    feat: 安装并配置unplugin-vue-define-options插件

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 使.vue文件setup方式支持调用defineOptions来定义组件名称
+import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), DefineOptions()],
 })
