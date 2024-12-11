99a5f27d2672b85daebd0258bd8eda9a821f01c9
v_jpch <v_jpch.digitalgd.com>
2024-12-11 12:51:05 +0800

    feat: 在play演示项目中安装ionicons5作为icon图标库并添加测试代码

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { AccessibilitySharp } from "@vicons/ionicons5";
+</script>
 
 <template>
-  <x-icon color="red" size="20">测试</x-icon>
+  <x-icon color="red" size="20">
+    <AccessibilitySharp />
+  </x-icon>
 </template>
 
 <style scoped></style>
